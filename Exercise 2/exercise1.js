44013342d3144e1c39e367971746adf7a7c9cc2c
KristofVerslegers <kristof.verslegers@benelux.amadeus.com>
2016-11-03 13:23:59 +0100

    Added exercise 2

---
+++ Exercise 2/exercise1.js
@@ -0,0 +1,29 @@
+/*
+ * Exercise 1
+ * ----------
+ * Complete the function “max”. This function takes 2 parameters and returns the largest number. 
+ * If one of the 2 parameters is not a number, return the String “Only numbers are allowed”.
+ *
+ * Complete the second function "retrieveAndDisplayHighestNumber" so it retrieves the numbers 
+ * from the 2 input fields and calls the "max" function with those 2 numbers.
+ * Display the highest number by calling the build-in "Alert" function.
+ *
+ * TIPS:
+ * - Use the function "isNumber" in this file to check if a parameter is a number
+ * - Retrieve the value of an HTML input field with this code: "document.getElementById(<html id>).value"
+ */
+
+ function max() {
+ 	// complete this
+ }
+
+ function retrieveAndDisplayHighestNumber(){
+ 	// complete this
+ }
+
+/**
+ * Use this function to check if the parameter is a number
+ */
+function isNumber(obj) { 
+	return !isNaN(parseFloat(obj));
+}
