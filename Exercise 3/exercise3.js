8eb1ce41c9625400126ebe87c2c8efea15166af7
KristofVerslegers <kristof.verslegers@benelux.amadeus.com>
2016-11-03 16:45:39 +0100

    Added exercise 3

---
+++ Exercise 3/exercise3.js
@@ -0,0 +1,19 @@
+/*
+ * Exercise 3
+ * ----------
+ * Complete the function 'calculateAverageGrade', so this funtion logs the average score and the corresponding grade of all the students.
+ * First we have to loop through all the students and calculate the average score and log this.
+ * Then we are going to log which grade is corresponding to this average score.
+ *
+ * Grade A -> score between 90 & 100
+ * Grade B -> score between 80 & 90
+ * Grade C -> score between 70 & 80
+ * Grade D -> score between 60 & 70
+ * Grade F -> score lower than 60
+ */
+
+function calculateAverageGrade() {
+	var students = [['Kristof', 80], ['Bart', 77], ['Tom', 88], ['Attila', 95], ['Ben', 68]];
+
+	// ADD YOUR CODE HERE
+}
