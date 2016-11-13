3e86150ffdfc77d9e6a42a8187c58c6a4a301149
KristofVerslegers <kristof.verslegers@benelux.amadeus.com>
2016-11-13 16:33:39 +0100

    Added Exercise 5

---
+++ Exercise 5/exercise5.js
@@ -0,0 +1,12 @@
+/* Exercise 5
+ * ----------
+ * Create a object called Ninja that contains a number property slices with value 0, a function slice which adds 1 to slices.
+ *
+ * Create a parent object ninjaTeacher containing a function teachToSlice. Make this object the parent of ninja. This function should alert a message ‘You are now ready to slice!’
+ *
+ * You have three buttons on your page.
+ * 
+ * When you click the first button an alert from the function teachToSlice should be shown.
+ * When you click the second button one slice should be added.
+ * When you click the third button an alert should be shown with the number of slices.
+ */
